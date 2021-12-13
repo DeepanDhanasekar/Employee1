1234f0db33e35942b73094a3df298224f46040fc
java <deepan2104d@gmail.com>
2021-12-13 22:31:28 +0530

    java

---
+++ src/org/name/java.java
@@ -0,0 +1,15 @@
+package org.name;
+
+public class java {
+public static void main(String[] args) {
+	int a[]=new int[5];
+	a[0]=10;
+	a[1]=20;
+	a[2]=30;
+	a[3]=40;
+	a[2]=50;
+	a[4]=60;
+	System.out.println(a[2]);
+
+}
+}
